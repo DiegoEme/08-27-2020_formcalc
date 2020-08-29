8b5f9fe0170794b9a6ac9527642f9af9e84bbf4f
Diego <soterg.uhdeia@gmail.com>
2020-08-29 12:02:05 -0500

    form y calc actualizados

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 const inputName = document.getElementById("inputName");
 const nombreMensaje = document.getElementById("nombreMensaje");
+
+const inputApellido = document.getElementById("inputApellido");
 const apellidoMensaje = document.getElementById("apellidoMensaje");
+
+const inputCorreo = document.getElementById("inputCorreo");
+const correoMensaje = document.getElementById("correoMensaje");
+
+const inputTel = document.getElementById("inputTel");
+const telMensaje = document.getElementById("telMensaje");
 
 function validarFormulario(event) {
   event.preventDefault();
   if (inputName.value == "") {
     inputName.classList.add("alert");
-    nombreMensaje.innerText = "Llene el campo nombre";
+    nombreMensaje.innerText = "Escriba su nombre";
   }
   if (inputApellido.value == "") {
+    inputApellido.classList.add("alert");
     apellidoMensaje.innerText = "Escriba su apellido";
   }
-  if (inputApellido.value == "") {
-    apellidoMensaje.innerText = "Escriba su apellido";
+  if (inputCorreo.value == "") {
+    inputCorreo.classList.add("alert");
+    correoMensaje.innerText = "Escriba su correo";
   }
-  window.location = "./calculadora.html";
+  if (inputTel.value == "") {
+    inputTel.classList.add("alert");
+    telMensaje.innerText = "Escriba su telefono";
+  }
+
+  if (
+    inputName.value == "" ||
+    inputApellido.value == "" ||
+    inputCorreo.value == "" ||
+    inputTel.value == ""
+  ) {
+    swal("¡Llene el formulario!");
+  } else {
+    swal("¡Buena mi perro!", "¡Ahora vamos a la calculadora!", "success");
+  }
+
+  setTimeout(function () {
+    window.location = "./calculadora.html";
+  }, 2000);
 }
